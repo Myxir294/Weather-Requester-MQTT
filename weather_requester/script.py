7f09d4b72236ac14a0eb8a3d2c972ecee321915c
Mikołaj Saramonowicz <252964@student.pwr.edu.pl>
2023-12-15 16:44:36 +0000

    press x to jason

--- weather_requester/script.py
+++ weather_requester/script.py
@@ -1,28 +1,34 @@
-import time
+from datetime import datetime
 import requests
 import os
 import json
+import time
 
-#os.environ["LOCATION"] = "333"
+os.environ["LOCATION"] = "323"
 
 class Weather_Requestor:
     def __init__(self, location):
         self.set_location = location
         self.url = "https://api.openaq.org/v2/latest/" + str(self.set_location) + "?limit=100&page=1&offset=0&sort=asc"
         self.headers = {"accept": "application/json"}
-        self.time = time.ctime()
+        self.time = datetime.now().isoformat()
+        self.dictionary = {}
 
     def output(self):
         self.response = requests.get(self.url, headers=self.headers)
         self.temp = json.loads(self.response.text)
-        self.time = time.ctime()
-        print("{\"location\": <" , self.temp["results"][0]["location"] + ">,")
-        print(" \"timestamp\": <" + str(self.time) + ">,")
-        print("\"values\":[")
-        print("{ " + self.temp["results"][0]["measurements"][0]["parameter"] + " : " + str(self.temp["results"][0]["measurements"][0]["value"]) + " " + self.temp["results"][0]["measurements"][0]["unit"] ) 
-        print("  " + self.temp["results"][0]["measurements"][1]["parameter"] + " : " + str(self.temp["results"][0]["measurements"][1]["value"]) + " " + self.temp["results"][0]["measurements"][1]["unit"] )
-        print("]}")
-        print(" ")
+        self.time = datetime.now().isoformat();
+        self.dictionary = {}
+        self.dictionary.update({"location" : self.temp["results"][0]["location"]})
+        self.dictionary.update({"timestamp" : str(self.time)})
+        values_dictionary = {}
+        for element in self.temp["results"][0]["measurements"]:
+            values_dictionary.update({element["parameter"] : element["value"]})
+            
+        self.dictionary.update({"values" : values_dictionary})
+
+        json_string = json.dumps(self.dictionary, skipkeys = True, allow_nan = True)
+        print(json_string)
         print(" ")
 
 myrequestor = Weather_Requestor(os.environ['LOCATION'])
