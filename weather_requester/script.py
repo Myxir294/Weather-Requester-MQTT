9e1b0c615face22613e44cedd5d2bdeaf48d2fea
Mikołaj Saramonowicz <252964@student.pwr.edu.pl>
2024-01-11 11:18:48 +0000

    aaaaaaamqtt

--- weather_requester/script.py
+++ weather_requester/script.py
@@ -3,41 +3,39 @@ import requests
 import os
 import json
 import time
+
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes 
 import ssl
 
-def on_message(client, userdata, message, properties=None):
-    print(" Received message " + str(message.payload)
-        + " on topic '" + message.topic
-        + "' with QoS " + str(message.qos))
-
-mytransport = 'tcp' # or 'websockets'
-
-client = mqtt.Client(client_id="myPy", transport=mytransport, protocol=mqtt.MQTTv5)
-
+client = mqtt.Client(client_id="student252964", transport='tcp', protocol=mqtt.MQTTv5)
 client.username_pw_set("student", "sys_wbud")
 client.tls_set(certfile=None,
                keyfile=None,
                cert_reqs=ssl.CERT_REQUIRED)
-    
-client.on_message = on_message;
 
-broker = '46.101.108.102' # eg. choosen-name-xxxx.cedalo.cloud
-myport = 1883
+def on_message(client, userdata, message, properties=None):
+   print(" Received message " + str(message.payload)
+        + " on topic '" + message.topic
+        + "' with QoS " + str(message.qos))
+
+client.on_connect = on_message
+   
 properties=Properties(PacketTypes.CONNECT)
-properties.SessionExpiryInterval=30*60 # in seconds
-client.connect(broker, port=myport, clean_start=mqtt.MQTT_CLEAN_START_FIRST_ONLY, properties=properties, keepalive=60);
+properties.SessionExpiryInterval=30*60 #sekundy
 
-client.loop_start();
+client.connect("46.101.108.102", 1883, clean_start=mqtt.MQTT_CLEAN_START_FIRST_ONLY, properties=properties, keepalive=60)
+
+client.loop_start()
 
 ################lab 2
 
 os.environ["LOCATION"] = "10566"
 
 class Weather_Requestor:
-    def __init__(self, location):
+    def __init__(self, location): 
         self.set_location = location
         self.url = "https://api.openaq.org/v2/latest/" + str(self.set_location) + "?limit=100&page=1&offset=0&sort=asc"
         self.headers = {"accept": "application/json"}
